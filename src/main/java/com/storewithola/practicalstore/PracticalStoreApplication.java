588f42bf0b136da7e5b8e4908beed6e6602bf7c3
Arab45 <horlakunleolayiwola@gmail.com>
2026-08-29 15:04:11 -0700

    first commit

---
+++ src/main/java/com/storewithola/practicalstore/PracticalStoreApplication.java
@@ -0,0 +1,13 @@
+package com.storewithola.practicalstore;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PracticalStoreApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PracticalStoreApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/storewithola/practicalstore/PracticalStoreApplicationTests.java
@@ -0,0 +1,13 @@
+package com.storewithola.practicalstore;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PracticalStoreApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
